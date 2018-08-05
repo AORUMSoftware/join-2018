9e19db7c92d2a09a94455fa5373b8ba1ef22a90d
JoaoRegis <joao.reegiis@gmail.com>
2018-08-05 16:52:28 -0300

    Functions and Returns

---
+++ Functions ES6.js
@@ -0,0 +1,23 @@
+function x(a, b) {
+    return a + b
+}
+
+const x = function(a, b) {
+    return a + b
+}
+
+const x = () => {
+    return 'Hello'
+}
+
+const x = a => {
+    return a + 1
+}
+
+const x = (a, b) => {
+    return a + b
+}
+
+// const
+// let
+// var
---
+++ Returns ES6.js
@@ -0,0 +1,19 @@
+const x = (a, b) => ( 
+
+    a + b
+
+)
+
+const y = (a, b) => {
+
+    return (
+        a + b
+    )
+
+}
+
+const z = (a, b) => a + b
+
+const c = (a, b) => { 
+    return a + b 
+}
